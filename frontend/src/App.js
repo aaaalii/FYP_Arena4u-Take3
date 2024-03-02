8fb5a408289e619e793c766b6bfa554a90c28897
aaaalii <ahmadmubashar373@gmail.com>
2024-03-02 22:12:54 +0500

    Progressing, not something big

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <BrowserRouter>
-        <div>
+        <div className={styles.layout}>
           <Navbar />
           <Routes>
-            <Route path="/" exact element={<Home />} />
+            <Route 
+              path="/"
+              exact 
+              element={
+              <div className={styles.main}>
+                <Home />
+              </div>
+              } />
           </Routes> 
           <Footer />
         </div>
